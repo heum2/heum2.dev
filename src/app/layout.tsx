55b44b5a94863d550e66a29d61c5cb3fc2a32014
heum2 <jwh6295@gmail.com>
2023-06-30 16:42:01 +0900

    feat: seo 적용

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,16 @@
 import { Providers } from "./providers";
 import "./globals.css";
 import { Noto_Sans_KR } from "next/font/google";
+import { Metadata } from "next";
 
 const notosans = Noto_Sans_KR({ weight: "700", subsets: ["latin"] });
 
-export const metadata = {
+export const metadata: Metadata = {
   title: "heum.dev",
   description: "개발 및 일기를 끄적이는 블로그",
+  verification: {
+    google: "wqdMVCQQf_90q8KKZXOk13hsMOSNhs6646sM8giTGpY",
+  },
 };
 
 export default function RootLayout({
