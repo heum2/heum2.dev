713a7fa2fd0eb3e2bea86c90ec7c92223105e6cc
heum2 <jwh6295@gmail.com>
2023-08-22 14:27:12 +0900

    fix: sitemap 변경

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: "https://heum2-dev.vercel.app/",
+  siteUrl: "https://heum2.dev/",
   changefreq: "daily",
   priority: 0.7,
   sitemapSize: 7000,
